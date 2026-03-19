3161b948991392da7f1617d92019ccb5af19efd9
daneeqx <daneeqx@gmail.com>
2026-03-19 13:55:51 +0200

    Update install.sh

--- .devcontainer/setup/common/install.sh
+++ .devcontainer/setup/common/install.sh
@@ -1,1 +1,11 @@
 #!/usr/bin/bash -e
+
+GITLAB_DOMAIN="${GITLABDOMAIN}"
+GITLAB_USER="${GITLABUSERNAME}"
+GITLAB_TOKEN="${GITLABTOKEN}"
+
+if [ -n "$GITLAB_TOKEN" ] && [ -n "$GITLAB_DOMAIN" ]; then
+    git config --system credential.helper store
+    echo "https://${GITLAB_USER}:${GITLAB_TOKEN}@${GITLAB_DOMAIN}" >> /etc/git-credentials
+    chmod 600 /etc/git-credentials
+fi
