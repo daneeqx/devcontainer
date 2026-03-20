c388aebbf45fad2788647eb46b67ec8c938291d0
daneeqx <daneeqx@gmail.com>
2026-03-20 13:56:28 +0200

    Update install.sh

--- .devcontainer/setup/common/install.sh
+++ .devcontainer/setup/common/install.sh
@@ -5,7 +5,18 @@ GITLAB_USER="${GITLABUSERNAME}"
 GITLAB_TOKEN="${GITLABTOKEN}"
 
 if [ -n "$GITLAB_TOKEN" ] && [ -n "$GITLAB_DOMAIN" ]; then
+    # Git credential store for HTTPS access
     git config --system credential.helper store
-    echo "https://${GITLAB_USER}:${GITLAB_TOKEN}@${GITLAB_DOMAIN}" >> /etc/git-credentials
+    echo "https://${GITLAB_USER}:${GITLAB_TOKEN}@${GITLAB_DOMAIN}" > /etc/git-credentials
     chmod 600 /etc/git-credentials
+
+    # .netrc in user home for Go module downloads (go mod uses ~/.netrc)
+    _REMOTE_USER_HOME="${_REMOTE_USER_HOME:-/home/${_REMOTE_USER:-vscode}}"
+    cat <<EOF > "${_REMOTE_USER_HOME}/.netrc"
+machine ${GITLAB_DOMAIN}
+login ${GITLAB_USER}
+password ${GITLAB_TOKEN}
+EOF
+    chown "${_REMOTE_USER:-vscode}:${_REMOTE_USER:-vscode}" "${_REMOTE_USER_HOME}/.netrc"
+    chmod 600 "${_REMOTE_USER_HOME}/.netrc"
 fi
